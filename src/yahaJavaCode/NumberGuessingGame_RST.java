1e4c68646575db38242eaccb611624daf6b19a6b
s510712 <s510712@FXH-4238177141>
2026-01-16 09:23:42 -0500

    RST Yahya. Not Complete

--- src/yahaJavaCode/NumberGuessingGame_RST.java
+++ src/yahaJavaCode/NumberGuessingGame_RST.java
@@ -12,11 +12,15 @@ import java.awt.event.ActionEvent;
 
 public class NumberGuessingGame_RST {
 
+	int points = 1000;
+	
 	private JFrame frame;
 	private JTextField numGuessTextField;
 	private JTextField betPointsTextField;
-	private JTextField pointsTextField;
 	private JTextField randomNumTextField;
+	private JTextField winLossTextField;
+	private JTextField pointsTextField;
+	private JButton exitButton;
 
 	/**
 	 * Launch the application.
@@ -57,7 +61,7 @@ public class NumberGuessingGame_RST {
 		
 		JLabel instructionLabel = new JLabel("This is a game where you bet points and guess a number 1-6");
 		instructionLabel.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		instructionLabel.setBounds(70, 43, 315, 14);
+		instructionLabel.setBounds(62, 32, 315, 14);
 		frame.getContentPane().add(instructionLabel);
 		
 		numGuessTextField = new JTextField();
@@ -78,40 +82,64 @@ public class NumberGuessingGame_RST {
 				int betPoints = 0;
 				int userGuess = 0;
 				int pointHolder = 0;
+				int pointsWin = 0;
+				int pointsLoss = 0;
+				
+				
+				// Total points tracker
+				int points1 = points;
+				points1 = Integer.parseInt(pointsTextField.getText());
+				
 				
 				// Get amount of points bet from user
-				int betPoints1 = Integer.parseInt(betPointsTextField.getText());
-				betPoints1 = betPoints;
+				int betPoints1 = betPoints;
+				betPoints1 = Integer.parseInt(betPointsTextField.getText());
+				
 				
 				// Get the number guessed by the user
-				int userGuess1 = Integer.parseInt(numGuessTextField.getText());
-				userGuess1 = userGuess;
+				userGuess = Integer.parseInt(numGuessTextField.getText());
 				
+			
+				
+								
+				// If user inputs number other then 1-6
+				if (userGuess < 1 || userGuess > 6) {
+                    winLossTextField.setText("Please enter a number between 1 and 6.");
+                
+                    randomNumTextField.setText("");
+                    return; // Stops the code here so the game doesn't run
+				}	
+					
 				// Random number
 				int n4 = 6;
 				int randomNum = (int) (Math.random() * n4) + 1;
 				
 				// Send the random number back to the GUI
 				randomNumTextField.setText(String.valueOf(randomNum));
-				
-				
+
 				// Decide who wins 
-				if (userGuess1 != randomNum){
+				if (userGuess != randomNum){
 					
 					pointHolder = betPoints1 / 2;
-					points = points - pointHolder;
+					pointsLoss = points - pointHolder;
 					
 					
-					pointsTextField.setText(String.valueOf(points));
+					pointsTextField.setText(String.valueOf(pointsLoss));
+					
+					winLossTextField.setText("You Lose");
 				}
 				
-				else if (userGuess1 == randomNum) {
+				else if (userGuess == randomNum) {
 					
 					pointHolder = betPoints1 * 2;
 					pointHolder = pointHolder - betPoints1;
-					points = points + pointHolder;
+					pointsWin = points + pointHolder;
 					
+					pointsTextField.setText(String.valueOf(pointsWin));
+					
+					winLossTextField.setText("You Win");
 				}
+				
 				
 					
 					
@@ -140,17 +168,45 @@ public class NumberGuessingGame_RST {
 		amountPointsLabel.setBounds(21, 80, 134, 14);
 		frame.getContentPane().add(amountPointsLabel);
 		
-		pointsTextField = new JTextField();
-		pointsTextField.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		pointsTextField.setBounds(149, 77, 86, 20);
-		frame.getContentPane().add(pointsTextField);
-		pointsTextField.setColumns(10);
-		
 		randomNumTextField = new JTextField();
 		randomNumTextField.setText("Dice Number Will Appear Here");
 		randomNumTextField.setFont(new Font("Times New Roman", Font.PLAIN, 11));
 		randomNumTextField.setBounds(265, 209, 159, 20);
 		frame.getContentPane().add(randomNumTextField);
 		randomNumTextField.setColumns(10);
+		
+		winLossTextField = new JTextField();
+		winLossTextField.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		winLossTextField.setBounds(28, 105, 210, 20);
+		frame.getContentPane().add(winLossTextField);
+		winLossTextField.setColumns(10);
+		
+		pointsTextField = new JTextField();
+		pointsTextField.setText("1000");
+		pointsTextField.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		pointsTextField.setBounds(152, 77, 86, 20);
+		frame.getContentPane().add(pointsTextField);
+		pointsTextField.setColumns(10);
+		
+		JLabel exitInstructionLabel = new JLabel("If you want to leave the program enter \"Exit\" in the guess area.");
+		exitInstructionLabel.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		exitInstructionLabel.setBounds(62, 52, 337, 14);
+		frame.getContentPane().add(exitInstructionLabel);
+		
+		exitButton = new JButton("Exit");
+		exitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			
+				// Exit button starts here
+				
+				System.exit();
+				
+				// Exit button ends here
+			
+			}
+		});
+		exitButton.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		exitButton.setBounds(300, 103, 89, 23);
+		frame.getContentPane().add(exitButton);
 	}
 }
